dd3ddd5cbdb7beca5055454ff9d316864a312bc8
Igor <Igor-87@mail.ru>
2023-02-16 19:46:36 +0300

    added slider on the second page

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,10 @@ import Form from "./modules/form";
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns : '.next', container: '.page'});
-
     slider.render();
+
+    const modulePageSlider  = new MainSlider({container:'.moduleapp', btns:'.next'});
+    modulePageSlider.render();
 
     const showUpSlider = new MiniSlider({
         container:'.showup__content-slider',
--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -42,28 +42,43 @@ export default class MainSlider extends Slider {
         this.showSlides(this.slideIndex += n);
     }
 
+    bindTriggers() {
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                this.plusSlides(1);
+                this.slides[this.slideIndex - 1].classList.add('fadeIn', 'animate__slow');
+            });
+            btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+                this.slides[this.slideIndex - 1].classList.add('fadeIn', 'animate__slow');
+            });
+        });
+
+        document.querySelectorAll('.prevmodule').forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(-1);
+            });
+        });
+        document.querySelectorAll('.nextmodule').forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(1);
+            });
+        });
+    }
+
     render() {
-        try {
+        if (this.container) {
             try {
                 this.hanson = document.querySelector('.hanson');
             } catch (e) { }
-
-
-            this.btns.forEach(btn => {
-                btn.addEventListener('click', () => {
-                    this.plusSlides(1);
-                    this.slides[this.slideIndex - 1].classList.add('fadeIn', 'animate__slow');
-                });
-                btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    this.slideIndex = 1;
-                    this.showSlides(this.slideIndex);
-                    this.slides[this.slideIndex - 1].classList.add('fadeIn', 'animate__slow');
-                });
-            });
             this.showSlides(this.slideIndex);
+            this.bindTriggers();
         }
-        catch(error) {
     }
-}
 }
