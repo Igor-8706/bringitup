05e4b1f1c87b3922554af1b94effd8e8b53a4325
Igor <Igor-87@mail.ru>
2023-02-16 21:00:39 +0300

    added video players

--- src/js/main.js
+++ src/js/main.js
@@ -7,14 +7,14 @@ import Form from "./modules/form";
 
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new MainSlider({btns : '.next', container: '.page'});
+    const slider = new MainSlider({ btns: '.next', container: '.page' });
     slider.render();
 
-    const modulePageSlider  = new MainSlider({container:'.moduleapp', btns:'.next'});
+    const modulePageSlider = new MainSlider({ container: '.moduleapp', btns: '.next' });
     modulePageSlider.render();
 
     const showUpSlider = new MiniSlider({
-        container:'.showup__content-slider',
+        container: '.showup__content-slider',
         prev: '.showup__prev',
         next: '.showup__next',
         activeClass: 'card-active',
@@ -23,27 +23,28 @@ window.addEventListener('DOMContentLoaded', () => {
     showUpSlider.init();
 
     const modulesSlider = new MiniSlider({
-        container:'.modules__content-slider',
+        container: '.modules__content-slider',
         prev: '.modules__info-btns .slick-prev',
         next: '.modules__info-btns .slick-next',
         activeClass: 'card-active',
         animate: true,
-        autoplay:true
+        autoplay: true
     });
     modulesSlider.init();
 
     const feedSlider = new MiniSlider({
-        container:'.feed__slider',
+        container: '.feed__slider',
         prev: '.feed__slider .slick-prev',
         next: '.feed__slider .slick-next',
         activeClass: 'feed__item-active'
     });
     feedSlider.init();
 
-    const player = new VideoPlayer('.showup .play','.overlay');
-    player.init();
+    new VideoPlayer('.showup .play', '.overlay').init();
+    new VideoPlayer('.module__video-item .play','.overlay').init();
 
-    new Difference('.officerold','.officernew','.officer__card-item').init();
+
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
 
 
