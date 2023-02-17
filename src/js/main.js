59ed1e7ab8cc3909916e43bab25fd5a2635e71c9
Igor <Igor-87@mail.ru>
2023-02-17 20:48:44 +0300

    added download files

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@ import VideoPlayer from "./modules/playVideo";
 import MiniSlider from "./modules/slider/slider-mini";
 import Difference from "./modules/difference";
 import Form from "./modules/form";
-
+import ShowInfo from "./modules/showInfo";
+import Download from "./modules/download";
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -47,7 +48,9 @@ window.addEventListener('DOMContentLoaded', () => {
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
 
+    new ShowInfo('.plus__content').init();
 
+    new Download('.download').init();
 
 
 });
